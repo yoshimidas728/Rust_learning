2d6c57902a85570e15fae66d4d6f23e517506821
yoshimidas728 <yoshimidas728@gmail.com>
2025-02-02 13:27:23 +0000

    ownership_branch_first

---
+++ ownership/target/debug/deps/ownership-b4a22640b16f70d9.d
@@ -0,0 +1,5 @@
+/root/Rust_learning/Rust_learning/ownership/target/debug/deps/ownership-b4a22640b16f70d9: src/main.rs
+
+/root/Rust_learning/Rust_learning/ownership/target/debug/deps/ownership-b4a22640b16f70d9.d: src/main.rs
+
+src/main.rs:
---
+++ ownership/target/debug/ownership.d
@@ -0,0 +1,1 @@
+/root/Rust_learning/Rust_learning/ownership/target/debug/ownership: /root/Rust_learning/Rust_learning/ownership/src/main.rs
