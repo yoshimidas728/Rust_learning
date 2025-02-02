d3f7f722ed63afd1fba30c5715ae080844dc6c3f
yoshimidas728 <yoshimidas728@gmail.com>
2025-02-02 13:35:30 +0000

    ownership_and_Function

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,6 +1,15 @@
-fn main() {
-    let s1 = String::from("hello");
-    let s2 = s1.clone();
+fn main(){
+    let s = String::from("hello");
+    takes_ownership(s);
 
-    println!("s1 = {s1}, s2 = {s2}");
+    let x =5;
+    makes_copy(x);
+}
+
+fn takes_ownership(some_string: String){
+    println!("{some_string}");
+}
+
+fn makes_copy(some_integer: i32){
+    println!("{some_integer}");
 }
